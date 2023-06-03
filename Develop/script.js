c7670de859a13a2287554ac3fb859a501032558d
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-06-03 16:23:21 -0400

    generatingpassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,17 @@
 // Assignment code here
+var numbers = 1234567890;
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialCharacters = "!@#$%^&*()_+";
+var password = "";
 
+function generatePassword() {
+  var passwordlength = prompt("How many characters you want to be for the password?");
+if (thelength < 8 && thelength > 128) {
+  alert("thepassword length is from 8 to 128 characters");
+  
+  console.log(passwordlength);
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
