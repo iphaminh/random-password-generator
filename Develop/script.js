774a65a823214ffb58bb405455e93b3a399fd1fe
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-06-05 13:59:34 -0400

    Altertall variables and making a prompt function

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,46 @@
 // Assignment code here
-var numbers = 1234567890;
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var specialCharacters = "!@#$%^&*()_+";
-var password = " ";
-
-function generatePassword() {
-  var passwordlength = prompt("How many characters you want to be for the password?");
-  // Changing the let or var order here or after the first if statement will effect which prompts will pop-up first 
-  let includeLowercase = confirm("lower case letters");
-  let includeUpercase = confirm("upper case letters");
-  let includenumbers = confirm("numbers");
-  let includespecial =confirm("special characters");
+var numbers = ["1", "2", "3", "4", "5", "6", "7","8","9","10"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+var emArr= [];
+var chaLength = 8;
   
-if (passwordlength < 8 || passwordlength > 128) {
-  alert("Password must be between 8 and 128 characters");
-  return;
-}
-if (includeLowercase === false && includeUpercase === false && includenumbers === false && includespecial === false) {
-  alert("At least one character type must be selected");
-  return;
-}
-}  
 
-// password += numbers[Math.floor(Math.random() * numbers)];
-// password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-// password += upperCase[Math.floor(Math.random() * upperCase.length)];
-// password += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+// function generatePassword() {
+//   var password = " ";
+//   password += numbers[Math.floor(Math.random() * numbers)];
+//   password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+//   password += upperCase[Math.floor(Math.random() * upperCase.length)];
+//   password += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+  
+//   for (var i = 8; i < passwordLength; i++) {
+//     if (passCombination.includes(password[i])) {
+//       password += passCombination.floor(Math.random() * passCombination.length)];
+//     } else {
+//       cpassCombination.push(password[i]);
+//     }
+  
+// }
+// }
+
+function getPrompt() {
+  var passwordlength = parseInt(prompt("How many characters you want to be for the password?")); //convert the password string into a number/ integer
+
+
+  let includeLowercase = "lower case letters";   // Changing the let or var order here or after the first if statement will effect which prompts will pop-up first 
+  let includeUpercase = "upper case letters";
+  let includenumbers = "numbers";
+  let includespecial = "special characters";
+
+    if (isNaN(chaLength) || chaLength < 8 || chaLength > 128) {
+      alert("Character length must be number and between 8 and 128 digits");
+    }
+    if (confirm(includeLowercase === false || includeUpercase === false || includenumbers === false || includespecial === false)) {
+      alert("At least one character type must be selected");
+      
+   }
+  }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -42,3 +56,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
